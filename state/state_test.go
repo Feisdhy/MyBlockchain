767caf874e97b54d6c1df874d8818ee178a58bd3
Feisdhy <1633898838@qq.com>
2023-06-25 15:14:55 +0800

    Update in 2023/6/25

--- state/state_test.go
+++ state/state_test.go
@@ -10,19 +10,19 @@ import (
 )
 
 func TestStateDB(t *testing.T) {
-	db, _ := rawdb.NewLevelDBDatabase(
+	db1, _ := rawdb.NewLevelDBDatabase(
 		DefaultLevelDBConfig.File,
 		DefaultLevelDBConfig.Cache,
 		DefaultOpenOptions.Handles,
 		DefaultLevelDBConfig.Namespace,
 		DefaultLevelDBConfig.Readonly)
-	defer db.Close()
+	defer db1.Close()
 
 	//db, _ := rawdb.Open(DefaultOpenOptions)
 	//defer db.Close()
 
-	sdb, _ := state.New(types.EmptyRootHash, state.NewDatabase(db), nil)
-	defer sdb.Database().TrieDB().Commit(sdb.IntermediateRoot(false), false)
+	sdb1, _ := state.New(types.EmptyRootHash, state.NewDatabase(db1), nil)
+	defer sdb1.Database().TrieDB().Commit(sdb1.IntermediateRoot(false), false)
 
 	//sdb.GetOrNewStateObject(common.HexToAddress("0xEf8801eaf234ff82801821FFe2d78D60a0237F97"))
 	//sdb.SetBalance(common.HexToAddress("0xEf8801eaf234ff82801821FFe2d78D60a0237F97"), big.NewInt(1000))
@@ -30,8 +30,8 @@ func TestStateDB(t *testing.T) {
 	//hash, _ := sdb.Commit(false)
 	//fmt.Println(hash)
 
-	balance := sdb.GetBalance(common.HexToAddress("0xEf8801eaf234ff82801821FFe2d78D60a0237F97"))
-	fmt.Println(balance)
+	balance1 := sdb1.GetBalance(common.HexToAddress("0xEf8801eaf234ff82801821FFe2d78D60a0237F97"))
+	fmt.Println(balance1)
 
 	//sdb.SetBalance(common.HexToAddress("0xEf8801eaf234ff82801821FFe2d78D60a0237F97"), big.NewInt(1000))
 }
